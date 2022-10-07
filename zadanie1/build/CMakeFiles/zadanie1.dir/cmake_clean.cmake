25c3c7fcf240a3af7ed29b0d87c030251b7a497a
MaciejPawluczuk <86221441+s22964@users.noreply.github.com>
2022-10-07 04:48:42 +0200

    Zadanie1
    Rozwiązanie zadania 1 z łamaniem tekstu na kawałki oraz Makefile

---
+++ zadanie1/build/CMakeFiles/zadanie1.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/zadanie1.dir/main.cpp.obj"
+  "CMakeFiles/zadanie1.dir/main.cpp.obj.d"
+  "libzadanie1.dll.a"
+  "zadanie1.exe"
+  "zadanie1.exe.manifest"
+  "zadanie1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/zadanie1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ zadanie1/build/CMakeFiles/zadanie1.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for zadanie1.
+# This may be replaced when dependencies are built.
