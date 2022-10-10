e4578a2d38edeedbe20db52fd86385aa81d3ba18
MaciejPawluczuk <86221441+s22964@users.noreply.github.com>
2022-10-10 21:18:53 +0200

    Dokończone zadanie

---
+++ zadanie2/README.md
@@ -0,0 +1,11 @@
+Wybrany temat to:
+https://en.wikipedia.org/wiki/3-partition_problem
+
+Działanie programu:
+1. Sprawdzenie czy podany zbiór S jest podzielny przez 3;
+2. Wyliczenie sumy dla każdego podzbioru - T;
+3. Przeanalizowanie każdej możliwej trójki liczb;
+4. W przypadku znalezienia trójki o sumie T, zapisuje ją w nowym zbiorze i usuwa z oryginalnego;
+5. W przypadku nieznalezienia trójki liczb o sumie T, zwraca komunikat z błędem;
+6. Powtarza kroki 3-5 do momentu utworzenia S/3 liczby zbiorów;
+7. Wyświetla rezultat;
--- zadanie2/main.cpp
+++ zadanie2/main.cpp
@@ -1,17 +1,48 @@
 #include <iostream>
 #include <vector>
-
-std::pair<std::vector<int>,bool>  check (std::vector<int> S,int T){
+std::vector<int> S;
+std::pair<std::vector<int>,bool>  check (int T){
     std::vector<int> v;
-    bool c;
-    return std::make_pair(v,c);
+    int m=S.size();
+    bool t= false;
+    for(int i=0;i<m;i++){
+        for (int j=i+1;j<m;j++){
+            for (int k=j+1;k<m;k++){
+                if(S[i]+S[j]+S[k]==T){
+                    t=true;
+                    v={S[i],S[j],S[k]};
+                    S.erase(S.begin()+k);
+                    S.erase(S.begin()+j);
+                    S.erase(S.begin()+i);
+                    return std::make_pair(v,t);
+                }
+            }
+        }
+    }
+
+    return std::make_pair(v,t);
 
 }
 
-int main() {
+int main(int argc, char ** argv) {
     using namespace std;
-    vector<int> S={20,23,25,30,49,45,27,30,30,40,22,19};
+
+    for(int i=1;i<argc;i++){
+        S.push_back(strtol(argv[i], NULL, 10));
+    }
+
     int m=S.size();
+    cout<<"Zbior S to: {";
+    for (int i = 0; i < m; i++)
+    {
+        cout << S[i];
+        if(i+1 < m){
+            cout<<",";
+        }
+    }
+    cout<<"}"<<endl;
+
+
     if (m%3!=0){
         cout<<"Zbior musi byc podzielny przez 3!";
     }else{
@@ -20,21 +51,35 @@ int main() {
             sum+=n;
         }
         int T=sum/(m/3);
-        cout<<T;
+        cout<<"Suma T="<<T<<endl;
+
         vector<vector<int>> ans;
         pair<vector<int>,bool> result;
-        for (int i=0;i<m;i++){
-            result=check(S,T);
+        for (int i=0;i<m/3;i++){
+            result=check(T);
             if(!result.second){
-                cout<<"nie mozna.";
+                cout<<"nie znaleziono 3 liczb o sumie T="+T<<endl;
                 break;
             }else{
                 ans.push_back(result.first);
             }
         }
+        if(result.second){
+        cout<<"Wynik:"<<endl;
+        for (int i = 0; i < ans.size(); i++)
+        {
+            cout<<"{";
+            for (int j = 0; j < ans[i].size(); j++)
+            {
+                cout << ans[i][j];
+                if(j+1 < ans[i].size()){
+                    cout<<",";
+                }
+            }
+            cout<<"}"<<endl;
 
-
-
+        }
+        }
     }
     return 0;
 }
