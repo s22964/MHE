1d9ff8eec3c22ef57ff3c778a473de3521be2f0e
MaciejPawluczuk <86221441+s22964@users.noreply.github.com>
2023-01-21 07:34:15 +0100

    Delete Zadanie7 directory

--- Zadanie7/script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#Run this in terminal
-#+ Command to compile c++ program. here i used common one
-g++ -pedantic-errors -Wall -c funkcjeImpl.cpp
-g++ -o program main.cpp funkcjeImpl.o
-exit 0
